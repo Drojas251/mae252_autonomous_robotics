45f923e6da0d1dd58765b63861f06a8ff583b087
HugeCoderGuy <alexscottlewis@gmail.com>
2021-11-15 13:19:30 -0800

    updated with world map

--- navigation/src/mapping.py
+++ navigation/src/mapping.py
@@ -11,28 +11,37 @@ import math
 # from visualization_msgs.msg import MarkerArray
 # from geometry_msgs.msg import PoseArray
 # from geometry_msgs.msg import Point
+from gazebo_msgs.msg import LinkStates
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import Twist
+
+#map dimensions
+X = 10 #meters
+Y = 2 # meters
+delta_x = 0.05 # meters
+delta_y = 0.05 # meters 
+world_map = np.zeros((int(Y/delta_y), int(X/delta_x)))
 
 
 class Mapping():
     
     def __init__(self):
         self.get_pc = rospy.Subscriber("/passthrough/output", PointCloud2, self.pc_callback)
-        self.get_pos = rospy.Subscriber("/gazebo/link_states", Pose, self.pos_callback)
+        #self.get_pc = rospy.Subscriber("/velodyne_points", PointCloud2, self.pc_callback)
+        self.get_pos = rospy.Subscriber("/gazebo/link_states", LinkStates, self.pos_callback)
         #self.get_twist = rospy.Subscriber("/gazebo/link_states", Twist, self.twist_callback)
     
     def pos_callback(self, data):
-        theta = data.orientation.z
-        pos_x = data.position.x
-        pos_y = data.position.y
-        print("angle: ", str(theta), " position:",str(pos_x)," ", str(pos_y))
+        # self.turtle_bot_pose = Pose()
+        # self.turtle_bot_pose = data.pose[5].position.x
+        theta = data.pose[5].orientation.z
+        pos_x = data.pose[5].position.x
+        pos_y = data.pose[5].position.y
+        #print("angle: ", str(theta), " position:",str(pos_x)," ", str(pos_y))
 
         #define transform matrix
-        self.h_transform = np.array([math.cos(theta),math.sin(theta),pos_x],
-                                [math.sin(theta),math.cos(theta),pos_y],
-                                [0         , 0        , 1   ])
+        self.h_transform = np.array([math.cos(theta),-math.sin(theta),pos_x, math.sin(theta),math.cos(theta),pos_y,0 , 0, 1]).reshape(3,3)
         #print(self.h_transform)
 
 
@@ -42,15 +51,22 @@ class Mapping():
         
         pc = ros_numpy.numpify(data)
          # create map
-        print(data)
+
         for i in range(0,pc.size):
             object_x = pc[i][0]
             object_y = pc[i][1]
-            robot_frame_object = np.array([object_x, object_y, 0]).reshape(1,3)
-            dist_away = math.sqrt(object_x^2+object_y^2)
-            print(dist_away)
-            if dist_away<=3:
+            robot_frame_object = np.array([object_x, object_y, 1]).reshape(3,1)
+            dist_away = math.sqrt(object_x**2+object_y**2)
+            if dist_away<=4:
                 worldtf = np.dot(self.h_transform,robot_frame_object)
+                #print("world:\n",str(worldtf),"\nrobot:\n",str(robot_frame_object))
+                # get element in the map where the point would belong
+                element_x = round(worldtf[0][0]/delta_x)
+                element_y = round(worldtf[1][0]/delta_y)
+
+                # store in map
+                world_map[[int(element_y) ], [int(element_x)]] = 1
+                print(world_map)
                 
 
 
@@ -62,13 +78,6 @@ class Mapping():
 def main():
     rospy.init_node('listener', anonymous=True)
 
-
-    #map dimensions
-    X = 10 #meters
-    Y = 2 # meters
-    delta_x = 0.05 # meters
-    delta_y = 0.05 # meters 
-    world_map = np.zeros((int(Y/delta_y), int(X/delta_x)))
     mapping = Mapping()
 
     try:
