d8b1ef2602b239dc723fefc93ce021f307ac6476
HugeCoderGuy <alexscottlewis@gmail.com>
2021-11-16 16:26:05 -0800

    fixed quaternion to euler angles

--- navigation/src/mapping.py
+++ navigation/src/mapping.py
@@ -17,14 +17,14 @@ def euler_from_quaternion(x, y, z, w):
     pitch is rotation around y in radians (counterclockwise)
     yaw is rotation around z in radians (counterclockwise)
     """
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1)
+    # t0 = +2.0 * (w * x + y * z)
+    # t1 = +1.0 - 2.0 * (x * x + y * y)
+    # roll_x = math.atan2(t0, t1)
     
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2)
+    # t2 = +2.0 * (w * y - z * x)
+    # t2 = +1.0 if t2 > +1.0 else t2
+    # t2 = -1.0 if t2 < -1.0 else t2
+    # pitch_y = math.asin(t2)
     
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
