91c2210adf17f3e50a81607ffa418c9ced1abac8
Adithya <adithyasrinath98@gmail.com>
2021-11-30 19:59:28 -0800

    added move package

---
+++ move/scripts/move.py
@@ -0,0 +1,12 @@
+import rospy
+from geometry_msgs.msg import Twist
+
+rospy.init_node('topic_publisher')
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+rate = rospy.Rate(2)
+move = Twist()
+move.linear.x = 0.5
+
+while not rospy.is_shutdown():
+    pub.publish(move)
+    rate.sleep()
